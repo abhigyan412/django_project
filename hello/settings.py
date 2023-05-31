baf45fc4cd05703a80767c7c6551b09b56a76ba0
abhigyan412 <74041526+abhigyan412@users.noreply.github.com>
2023-05-31 23:11:26 +0530

    Update settings.py

--- hello/settings.py
+++ hello/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*c4xhmws*w$-bkpgf=d)ow74yxns#%-zmcd!zcwczzkk@%7g3)'
+SECRET_KEY = os.environ.get('SECRET_KEY', default='')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+EBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
