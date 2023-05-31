d54c86c301a8ae955e1bd5632dbaf863eb3071df
abhigyan412 <74041526+abhigyan412@users.noreply.github.com>
2023-05-31 21:36:02 +0530

    d

--- hello/settings.py
+++ hello/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'render.apps.RenderConfig'
     'home.apps.HomeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
