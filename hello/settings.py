c2d2567bb90d1274a05a93c5489aa7440aa0b713
abhigyan412 <74041526+abhigyan412@users.noreply.github.com>
2023-06-01 00:15:52 +0530

    s

--- hello/settings.py
+++ hello/settings.py
@@ -123,6 +123,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+
+
+STATIC_ROOT = "app-root/repo/wsgi/static"
+
 STATIC_URL = '/static/'
 
 # Default primary key field type
