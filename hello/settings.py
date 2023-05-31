b84b24f7b5553c1ec8feec32f388e9fadbbde929
abhigyan412 <74041526+abhigyan412@users.noreply.github.com>
2023-06-01 00:08:21 +0530

    g

--- hello/settings.py
+++ hello/settings.py
@@ -131,5 +131,5 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 ## added manually
 STATICFILES_DIRS = [
-    BASE_DIR / "static"
+    os.path.join(BASE_DIR, 'static')
 ]
