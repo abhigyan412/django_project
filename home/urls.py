bc84c1421b6294c8f5ac10b5640373f2c7fc15f2
abhigyan412 <74041526+abhigyan412@users.noreply.github.com>
2023-05-31 21:13:11 +0530

    nn

--- home/urls.py
+++ home/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from home import views
 urlpatterns = [
-    path("homepage", views.index, name='homepage'),
+    path("", views.index, name='homepage'),
     path("about",views.about, name='about'),
     path("services",views.services, name='services'),
     path("contact",views.contact, name ='contact'),
