8bda5117a70e7a405905e032234e76df9f6f32ac
abhigyan412 <74041526+abhigyan412@users.noreply.github.com>
2023-06-14 22:22:15 +0530

    q

--- home/urls.py
+++ home/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path("about",views.about, name='about'),
     path("services",views.services, name='services'),
     path("contact",views.contact, name ='contact'),
+    path("someone",views.someone, name ='someone'),
+
 ]
