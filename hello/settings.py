eee017536854f5fa3ac78a5e81f332cbf51ed412
abhigyan412 <74041526+abhigyan412@users.noreply.github.com>
2024-06-13 17:46:32 +0530

    m

--- hello/settings.py
+++ hello/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', default='')
+SECRET_KEY = os.environ.get('SECRET_KEY', default='#')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['creams200.onrender.com']
+ALLOWED_HOSTS = ['creams200.onrender.com'  , '127.0.0.1']
 
 
 # Application definition
