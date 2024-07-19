24760c04e1fc3bb3605a8157e13b254219f29355
Abhigyan Tripathi <74041526+abhigyan412@users.noreply.github.com>
2024-07-19 17:18:09 +0530

    m

--- home/models.py
+++ home/models.py
@@ -8,7 +8,7 @@ class Contact(models.Model):
        desc=models.TextField(max_length=250)
 
 class signup(models.Model):
-      contact  = models.IntegerField(null = True )    
+      contact  = models.IntegerField(null = False  , default=False)    
       password = models.CharField(max_length = 15)
 
 
