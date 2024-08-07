6ab852410202a0adb67f3cabe726ef1ccf8b0025
Abhigyan Tripathi <74041526+abhigyan412@users.noreply.github.com>
2024-08-07 11:26:50 +0530

    m

--- hello/settings.py
+++ hello/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='#')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ '.vercel.app']
+ALLOWED_HOSTS = [ '.vercel.app' , '127.0.0.1']
 
 
 # Application definition
