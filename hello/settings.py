91fc7da79f4a38b4ba821079570cf295b67d07ec
Abhigyan Tripathi <74041526+abhigyan412@users.noreply.github.com>
2024-08-09 20:38:33 +0530

    m

--- hello/settings.py
+++ hello/settings.py
@@ -14,8 +14,8 @@ from pathlib import Path
 
 from django.contrib.messages import constants as messages
 import os 
-from python-dotenv import load_dotenv
-load_dotenv()
+#from python-dotenv import load_dotenv
+#load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
