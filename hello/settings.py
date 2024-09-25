ea020eeba723e1f76eb5f54e223c10d4a622888f
Abhigyan Tripathi <74041526+abhigyan412@users.noreply.github.com>
2024-09-25 10:38:34 +0530

    Update settings.py

--- hello/settings.py
+++ hello/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', default='#')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [ '.vercel.app' , '127.0.0.1']
 
