c12a65bc0247e57b4021a25264100961759af582
Abhigyan Tripathi <74041526+abhigyan412@users.noreply.github.com>
2024-10-05 11:41:04 +0530

    m

--- hello/settings.py
+++ hello/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='#')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ 'https://icecreams4444.onrender.com' , '127.0.0.1']
+ALLOWED_HOSTS = [ 'https://icecreams9999.onrender.com' , '127.0.0.1']
 
 
 # Application definition
